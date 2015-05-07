761c0d2d6340642ebd9e40fc563802751d99a0ef
Dallas Cook <dallas@pharmedio.com>
2015-05-07 16:21:52 -0500

    Update to node implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = 3010;
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -7,6 +8,6 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen('3010', function() {
-	console.log('Node is listening.');
+app.listen(port, function() {
+	console.log('You may now browse to http://localhost:'+port+'.');
 });
